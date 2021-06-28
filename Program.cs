0e9151e0d4f2327202cb42eab36f1fc55d47a847
Bomber <dilvir@live.de>
2021-06-28 17:39:18 +0200

    Init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BombBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
