07133625ff98adc27e17b1a8e33cec6154a84b23
Bomber <dilvir@live.de>
2021-07-01 23:50:50 +0200

    Added more Handlers, trying to build a bot to manage roles.

--- Events/DiscordEventHandler.cs
+++ Events/DiscordEventHandler.cs
@@ -1,4 +1,5 @@
 ﻿using DSharpPlus;
+using DSharpPlus.Entities;
 using DSharpPlus.EventArgs;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -20,11 +21,42 @@ namespace BombBot.Events
         {
             if (e.Message.Content.ToLower().StartsWith("ping"))
                 await e.Message.RespondAsync("pong!");
+            if (e.Message.Content.ToLower().StartsWith("!removerole"))
+            {
+                if (e.MentionedRoles.Count != 2)
+                {
+                    await e.Message.RespondAsync("Invalid amount of Roles mentioned.");
+                }
+                else
+                {
+                    List<DiscordRole> x = e.MentionedRoles.ToList<DiscordRole>();
+                    DiscordRole hasthisRole = x.First();
+                    DiscordRole getsremovedRole = x.Last();
+                    string temp = String.Format("Detected two Roles. {0} & {1}", hasthisRole.Mention, getsremovedRole.Mention);
+
+                    await GetMember(s, e);
+                    
+                }
+            }
         }
+        public async Task ReactionAddedHandler(DiscordClient s, MessageReactionAddEventArgs e)
+        {
+            String temp = String.Format("Detected <:test:{0}> Reaction in <#{1}>\n {2}", e.Emoji.Id, e.Channel.Id, e.Emoji.Url);
+            await e.Message.Channel.SendMessageAsync(temp);
+        }
+
+        public async Task GetMember(DiscordClient s, MessageCreateEventArgs e)
+        {
+            await e.Guild.GetAllMembersAsync();
+        }
+
+        #region addEventHandler
         public async Task setupAsync()
         {
             discord.MessageCreated += MessageCreatedHandler;
+            discord.MessageReactionAdded += ReactionAddedHandler;
         }
+        #endregion
 
     }
 }
