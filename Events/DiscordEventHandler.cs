cb67968231351766b46e1bacb3cf712d539b563a
Bomber <dilvir@live.de>
2021-07-04 14:58:04 +0200

    Bot can now check Member Roles and compare both mentioned roles.

--- Events/DiscordEventHandler.cs
+++ Events/DiscordEventHandler.cs
@@ -17,11 +17,14 @@ namespace BombBot.Events
         {
             discord = service.GetRequiredService<DiscordClient>();
         }
+
+
+
         public async Task MessageCreatedHandler(DiscordClient s, MessageCreateEventArgs e)
         {
             if (e.Message.Content.ToLower().StartsWith("ping"))
                 await e.Message.RespondAsync("pong!");
-            if (e.Message.Content.ToLower().StartsWith("!removerole"))
+            if (e.Message.Content.ToLower().StartsWith("!r"))
             {
                 if (e.MentionedRoles.Count != 2)
                 {
@@ -29,25 +32,51 @@ namespace BombBot.Events
                 }
                 else
                 {
-                    List<DiscordRole> x = e.MentionedRoles.ToList<DiscordRole>();
-                    DiscordRole hasthisRole = x.First();
-                    DiscordRole getsremovedRole = x.Last();
-                    string temp = String.Format("Detected two Roles. {0} & {1}", hasthisRole.Mention, getsremovedRole.Mention);
-
-                    await GetMember(s, e);
-                    
+                    await RemoveRoles(s, e);
                 }
             }
         }
         public async Task ReactionAddedHandler(DiscordClient s, MessageReactionAddEventArgs e)
         {
-            String temp = String.Format("Detected <:test:{0}> Reaction in <#{1}>\n {2}", e.Emoji.Id, e.Channel.Id, e.Emoji.Url);
+            String temp = String.Format("Detected {0} Reaction from {1} in <#{2}>\n {3}", e.Emoji.Name, e.User.Mention, e.Channel.Id, e.Emoji.Url);
             await e.Message.Channel.SendMessageAsync(temp);
         }
 
-        public async Task GetMember(DiscordClient s, MessageCreateEventArgs e)
+        public async Task RemoveRoles(DiscordClient s, MessageCreateEventArgs e)
         {
-            await e.Guild.GetAllMembersAsync();
+            List<DiscordRole> x = e.MentionedRoles.ToList<DiscordRole>();
+            DiscordRole hasthisRole = x.First();
+            DiscordRole getsremovedRole = x.Last();
+            int i = 0;
+            string memberlist = "";
+
+            var members = GetMembers(s, e);
+            foreach (DiscordMember member in members.Result)
+            {
+
+                if (member.IsBot)
+                {
+                    var allRoles = member.Roles.ToList<DiscordRole>();
+                    if (allRoles.Contains(e.MentionedRoles.First<DiscordRole>()) && allRoles.Contains(e.MentionedRoles.Last<DiscordRole>()))
+                    {
+                        string temp = String.Format("User {0} contains {1} & {2}", member.Username, e.MentionedRoles.First<DiscordRole>().Mention, e.MentionedRoles.Last<DiscordRole>().Mention);
+                        await e.Message.RespondAsync(temp);
+                    }
+                }
+                else
+                {
+                    memberlist += "\n" + member.Username;
+                }
+
+            }
+            //await e.Message.RespondAsync(memberlist + "\nTotal Users: " + members.Result.Count());
+
+        }
+
+        public async Task<IReadOnlyCollection<DiscordMember>> GetMembers(DiscordClient s, MessageCreateEventArgs e)
+        {
+            var members = e.Guild.GetAllMembersAsync().Result;
+            return members;
         }
 
         #region addEventHandler
